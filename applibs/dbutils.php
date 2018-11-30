2d76b88e01c9d9b6d635ded29863c34c9cfe3d06
Daniel Rudy <dr2867.business@pacbell.net>
2018-11-29 21:47:59 -0800

    Update dbutils.php

--- applibs/dbutils.php
+++ applibs/dbutils.php
@@ -10,6 +10,7 @@ PHP Web Application Database Utilities
 */
 
 const BASEDIR2 = '../libs/';
+require_once BASEDIR2 . 'error.php';
 require_once BASEDIR2 . 'database.php';
 require_once BASEDIR2 . 'dbaseconf.php';
 require_once BASEDIR2 . 'dbaseuser.php';
@@ -70,6 +71,54 @@ function getUserName($userid)
 	}
 
 	return $rxa;
+}
+
+// Calculates a letter grade based on the scale.
+function caluclateGrade($grade, $scale)
+{
+	global $CONFIGVAR;
+	global $dbapp;
+	global $herr;
+
+	$rxa = $dbapp->queryGradescale($scale);
+	if ($rxa == false)
+	{
+		if ($herr->checkState())
+			handleError($herr->errorGetMessage());
+		else
+			handleError('Database Error: Missing grade scale record.');
+	}
+
+	switch($CONFIGVAR['gradescale_mode']['value'])
+	{
+		case 0:
+			if ($rxa['grade_ap'] > $rxa['grade_a'])
+			{
+				if ($grade >= $rxa['grade_ap']) return 'A+';
+			}
+			if ($grade >= $rxa['grade_a']) return 'A';
+			if ($grade >= $rxa['grade_am']) return 'A-';
+			if ($grade >= $rxa['grade_bp']) return 'B+';
+			if ($grade >= $rxa['grade_b']) return 'B';
+			if ($grade >= $rxa['grade_bm']) return 'B-';
+			if ($grade >= $rxa['grade_cp']) return 'C+';
+			if ($grade >= $rxa['grade_c']) return 'C';
+			if ($grade >= $rxa['grade_cm']) return 'C-';
+			if ($grade >= $rxa['grade_dp']) return 'D+';
+			if ($grade >= $rxa['grade_d']) return 'D';
+			if ($grade >= $rxa['grade_dm']) return 'D-';
+			return 'F';
+			break;
+		case 1:
+			if ($grade >= $rxa['grade_a']) return 'A';
+			if ($grade >= $rxa['grade_b']) return 'B';
+			if ($grade >= $rxa['grade_c']) return 'C';
+			if ($grade >= $rxa['grade_d']) return 'D';
+			return 'F';
+			break;
+		default:
+			handleError('Database Error: Invalid grade scale mode.');
+	}
 }
 
 
